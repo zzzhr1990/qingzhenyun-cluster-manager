136dcbc2cc481a14f51b26a61fc4dc9ff6e04d0c
古娜 <shiti08@126.com>
2017-05-08 08:47:26 +0800

    Initial commit

---
+++ src/main/java/com/qingzhenyun/QingzhenyunClusterManagerApplication.java
@@ -0,0 +1,12 @@
+package com.qingzhenyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingzhenyunClusterManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QingzhenyunClusterManagerApplication.class, args);
+	}
+}
